e799fabe9f62e2821ebf1ee8df47bb17b68ca5b1
Trevis Wamuthenya <trevismurithi@gmail.com>
2026-05-11 19:29:50 +0300

    uncomment A file

--- src/demo/A.tsx
+++ src/demo/A.tsx
@@ -21,6 +21,19 @@ function dummyFunction2() {
     return 10;
 }
 
+function dummyFunction2() {
+    return 10;
+}
+
+function dummyFunction2() {
+    return 10;
+}
+
+function dummyFunction2() {
+    return 10;
+}
+
+
 console.warn(dummyFunction2());
 console.error(dummyFunction2());
  console.log(dummyFunction2());
